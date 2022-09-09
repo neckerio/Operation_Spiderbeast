8d9afd6d4504a374579fe48b02022575e29c38d5
necker <github@necker.io>
2022-09-09 16:27:39 -0700

    tags block requires an equals sign

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  tags {
+  tags = {
     Name = "vpc-main"
   }
 }
@@ -17,7 +17,7 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
-  tags {
+  tags = {
     Name = "subnet-main"
   }
 }
