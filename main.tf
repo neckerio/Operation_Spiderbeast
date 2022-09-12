ebc34fc153532ca6148b0bfec2f931826a498f8c
necker <github@necker.io>
2022-09-12 16:09:49 -0700

    Testing Terraform with added Route Table

--- main.tf
+++ main.tf
@@ -30,3 +30,16 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
+#Create route tables for public and private subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name      = "public_rtb"
+  }
+}
+
