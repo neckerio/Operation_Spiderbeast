041f4f9c1663b809bb62aea68f3f2639cdadc294
necker <github@necker.io>
2022-09-15 20:58:57 -0700

    ICMP fix: this should work...forgot to add security group to ec2 block... oh boy

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ resource "aws_instance" "web_server" {
   ami           = data.aws_ami.rhel_8.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.ingress_ssh.id]
+  vpc_security_group_ids = [aws_security_group.ingress_ssh.id, aws_security_group.ping.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.generated.key_name
 
